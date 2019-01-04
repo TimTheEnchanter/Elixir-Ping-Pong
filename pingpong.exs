4baf237fc30e2b77edc996cc1a66ab8e68d63c4d
James Parrott <japarrott@shaw.ca>
2019-01-03 23:26:25 -0700

    Main file
    Still need to finish some stuff on this and comment out a few things.

---
+++ pingpong.exs
@@ -0,0 +1,32 @@
+defmodule PingPong do
+  def ready do
+    receive do
+      {sender, action, 11} ->
+        IO.puts("#{11} from #{inspect(sender)}, sent #{inspect(action)}")
+        IO.puts("Game Over")
+
+      {sender, action, turn} ->
+        hit_to(sender, :pong, turn + 1)
+        ready
+    end
+  end
+
+  def hit_to(opponent_id, action, turn) do
+    IO.puts("#{turn}. hit_to #{inspect(opponent_id)} #{inspect(action)}")
+    send(opponent_id, {player_pid, action, turn})
+  end
+
+  def player_pid do
+    self
+  end
+end
+
+player_1 = self
+player_2 = spawn(PingPong, :ready, [])
+
+IO.puts("player_1: #{inspect(player_1)}")
+IO.puts("player_2: #{inspect(player_2)}")
+
+PingPong.hit_to(player_2, :ping, 1)
+
+PingPong.ready()
